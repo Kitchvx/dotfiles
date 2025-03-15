c77b307a83108daad329109128ed46ef8c985ee6
Nathan Kitching <nathk.nk1214@gmail.com>
2025-03-15 19:31:39 +0000

    fixed symlink for xinitrc & gitconfig

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 ln -s /home/$USER/.dotfiles/bash/.bashrc ~/.bashrc
 ln -s /home/$USER/.dotfiles/bash/.bash_profile ~/.bash_profile
-ln -s .xinitrc ~/.xinitrc
-ln -s .gitconfig ~/.gitconfig
+ln -s /home/$USER/.dotfiles/.xinitrc ~/.xinitrc
+ln -s /home/$USER/.dotfiles/.gitconfig ~/.gitconfig
